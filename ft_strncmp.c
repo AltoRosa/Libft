15ce29e3ea3dd66f384bdd55bd9d9acce89844a5
Rosana MIRALLES <rmiralle@e3r7p3.42.fr>
2017-11-15 15:49:42 +0100

    strn machin dans la poche!

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncy.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmiralle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/15 14:17:37 by rmiralle          #+#    #+#             */
-/*   Updated: 2017/11/15 14:59:44 by rmiralle         ###   ########.fr       */
+/*   Created: 2017/11/15 15:02:26 by rmiralle          #+#    #+#             */
+/*   Updated: 2017/11/15 15:49:25 by rmiralle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dst, const char *src, size_t n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
 
 	i = 0;
-	while (i < n)
-	{
-		dst[i] = src[i];
+	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
-	}
-	return (dst);
+	while (i < n)
+		return (s1[i] - s2[i]);
+	return(0);
 }
